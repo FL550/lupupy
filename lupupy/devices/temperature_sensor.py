43c688fb30a406449d354ca0c6296dbefb11ce51
FL550 <glacial0124@posteo.com>
2024-09-05 12:49:33 +0200

    Failure case when status reports still none

--- lupupy/devices/temperature_sensor.py
+++ lupupy/devices/temperature_sensor.py
@@ -6,6 +6,7 @@ import re
 regex_temp = re.compile("{WEB_MSG_TS_DEGREE}(\d+\.\d+)")
 regex_humi = re.compile("{WEB_MSG_RH_HUMIDITY}(\d+)")
 
+
 class LupusecTemperatureSensor(LupusecDevice):
     """Class to represent a temperature and humidity sensor."""
 
@@ -13,11 +14,11 @@ class LupusecTemperatureSensor(LupusecDevice):
     def status(self):
         #     """
         #     Get sensor state.
-
+        print(self._json_state)
         m = regex_temp.search(self._json_state.get("status"))
-        self._temperature = m.group(1)
+        self._temperature = m.group(1) if m else None
         m = regex_humi.search(self._json_state.get("status"))
-        self._humidity = m.group(1)
+        self._humidity = m.group(1) if m else None
         return f"{self.temperature} {self.humidity}"
 
     @property
